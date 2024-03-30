9873f44c296ba260276578bd978aa448a44e1db5
Murad <baghmr24@gmail.com>
2024-03-30 12:30:23 +0400

    Commit 3

--- NoteBook.java
+++ NoteBook.java
@@ -1,51 +1,17 @@
-import java.util.*;
+import java.util.List;
 
 public class NoteBook {
-    public static void main(String[] args) {
-        Map<Integer, Integer> m1 = new HashMap<>(); // ОЗУ, ЖД
-        Map<String, String> m2 = new HashMap<>(); // Операционная система, цвет
-        Map<Map<Integer, Integer>, Map<String, String>> laptops = new HashMap<>();
-        String[] criteries = new String[] {"ОЗУ", "Объём ЖД", "Операционная система", "Цвет", "Поиск", "Закрыть программму"};
+    private final List<Object> noteBook;
 
-        getItem(criteries);
+    public NoteBook(List<Object> noteBook) {
+        this.noteBook = noteBook;
     }
 
+    public void add(Object itemObject) {
+        noteBook.add(itemObject);
+    }
 
-    static void getItem(String[] criteries) {
-        System.out.println("Выберите один из пунктов: ");
-        Scanner scanner = new Scanner(System.in);
-        int num = 1;
-        for (String item: criteries) {
-            System.out.print(num  + " " + item + "\n");
-            num ++;
-        }
-        int index = scanner.nextInt();
-        if (index > criteries.length) {
-            switch (index) {
-                case 1:
-                    System.out.println("Введите размер ОЗУ: ");
-                    Integer RAM = scanner.nextInt();
-                    break;
-                case 2:
-                    System.out.println("Введите объём ЖД: ");
-                    Integer HDD = scanner.nextInt();
-                    break;
-                case 3:
-                    System.out.println("Введите операционную систему: ");
-                    String operatingSystem = scanner.next();
-                    break;
-                case 4:
-                    System.out.println("Введите цвет: ");
-                    String colour = scanner.next();
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    System.out.println("Программа успешно закрыта!");
-                default:
-                    System.out.println("Такого пункта нет!");
-                    getItem(criteries);
-            }
-        }
+    public void printNoteBook() {
+        System.out.println(noteBook);
     }
 }
